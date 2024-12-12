90ad91869bf014ae42b3d1c90094aec6b7e55c1d
Manuel de la Peña <mdelapenya@gmail.com>
2024-12-12 13:59:18 +0100

    chore: define SQL database estructure

---
+++ internal/database/testdata/00-schema.sql
@@ -0,0 +1,18 @@
+CREATE TABLE pokemons (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    type VARCHAR(50) NOT NULL,
+    hp INT NOT NULL,
+    attack INT NOT NULL,
+    defense INT NOT NULL
+);
+
+CREATE TABLE battles (
+    id SERIAL PRIMARY KEY,
+    pokemon1_id INT NOT NULL,
+    pokemon2_id INT NOT NULL,
+    winner_id INT NOT NULL,
+    FOREIGN KEY (pokemon1_id) REFERENCES pokemons (id),
+    FOREIGN KEY (pokemon2_id) REFERENCES pokemons (id),
+    FOREIGN KEY (winner_id) REFERENCES pokemons (id)
+);
