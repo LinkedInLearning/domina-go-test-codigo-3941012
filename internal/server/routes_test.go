a5335805ac77c2095485868e7f443cca68c0e239
Manuel de la Peña <mdelapenya@gmail.com>
2024-12-17 06:44:13 +0100

    chore: init routes in tests

--- internal/server/routes_test.go
+++ internal/server/routes_test.go
@@ -6,7 +6,6 @@ import (
 	"testing"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/basicauth"
 )
 
 func TestHandler(t *testing.T) {
@@ -15,36 +14,33 @@ func TestHandler(t *testing.T) {
 	// Inject the Fiber app into the server
 	s := &FiberServer{App: app}
 
-	// Apply Basic Auth middleware, only test is allowed
-	s.App.Use(basicauth.New(basicauth.Config{
-		Users: map[string]string{
-			"test": "test",
-		},
-	}))
+	s.RegisterFiberRoutes()
 
-	// Define a route in the Fiber app
-	app.Get("/", s.HelloWorldHandler)
-	// Create a test HTTP request
-	req, err := http.NewRequest("GET", "/", nil)
-	req.Header.Set("Authorization", "Basic dGVzdDp0ZXN0") // test:test in base64
-	if err != nil {
-		t.Fatalf("error creating request. Err: %v", err)
-	}
-	// Perform the request
-	resp, err := app.Test(req)
-	if err != nil {
-		t.Fatalf("error making request to server. Err: %v", err)
-	}
-	// Your test assertions...
-	if resp.StatusCode != http.StatusOK {
-		t.Errorf("expected status OK; got %v", resp.Status)
-	}
-	expected := "{\"message\":\"Welcome to Pokemon Battle!\",\"username\":\"test\"}"
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		t.Fatalf("error reading response body. Err: %v", err)
-	}
-	if expected != string(body) {
-		t.Errorf("expected response body to be %v; got %v", expected, string(body))
-	}
+	t.Run("get/", func(t *testing.T) {
+		// Create a test HTTP request
+		req, err := http.NewRequest("GET", "/", nil)
+		req.Header.Set("Authorization", "Basic YXNoOmtldGNodW0=") // ash:ketchum in base64
+		if err != nil {
+			t.Fatalf("error creating request. Err: %v", err)
+		}
+
+		// Perform the request
+		resp, err := app.Test(req)
+		if err != nil {
+			t.Fatalf("error making request to server. Err: %v", err)
+		}
+		// Your test assertions...
+		if resp.StatusCode != http.StatusOK {
+			t.Errorf("expected status OK; got %v", resp.Status)
+		}
+
+		expected := "{\"message\":\"Welcome to Pokemon Battle!\",\"username\":\"ash\"}"
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			t.Fatalf("error reading response body. Err: %v", err)
+		}
+		if expected != string(body) {
+			t.Errorf("expected response body to be %v; got %v", expected, string(body))
+		}
+	})
 }
